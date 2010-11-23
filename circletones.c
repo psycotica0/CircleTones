0a1570be1af2e481d935b3b8a242f627a4f80f35
Christopher Vollick <psycotica0@gmail.com>
2010-11-22 20:57:17 -0500

    Movement.
    Now the circle gets bigger for a few seconds, then is gone.
    Also, so colours changed...

--- circletones.c
+++ circletones.c
@@ -4,19 +4,40 @@
 #include <math.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_gfxPrimitives.h"
+#include "SDL/SDL_framerate.h"
+
+void drawFrame(SDL_Surface* screen, int radius) {
+	static const int x = 320;
+	static const int y = 240;
+	static const Uint32 borderColour = 0x000000FF;
+	static const Uint32 fillColour = 0xAA00AAFF;
+	static const Uint32 backgroundColour = 0xAA44AAFF;
+
+	/* For no reason the filledRectangle is called box */
+	boxColor(screen, 0, 0, 640, 480, backgroundColour);
+
+	filledCircleColor(screen, x, y, radius + 4, borderColour);
+	filledCircleColor(screen, x, y, radius, fillColour);
+}
+
+#define FPS 60
 
 int main (int argc, char** argv) {
 	SDL_Surface *screen;
-	int i;
+	FPSmanager fpsm;
+	long i;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE | SDL_DOUBLEBUF);
-	/* I was trying to figure out how to make a line with multiple pixels. Then I realized I just make a bigger circle it sits on... */
-	filledCircleRGBA(screen, 300, 300, 53, 0x00, 0xFF, 0x00, 0xFF);
-	filledCircleRGBA(screen, 300, 300, 50, 0xFF, 0x00, 0xFF, 0xFF);
-	SDL_Flip(screen);
-	SDL_Delay(2000);
+	SDL_initFramerate(&fpsm);
+	SDL_setFramerate(&fpsm, FPS);
+
+	for(i=0; i <= FPS * 4; i++) {
+		drawFrame(screen, i);
+		SDL_Flip(screen);
+		SDL_framerateDelay(&fpsm);
+	}
 
 	SDL_Quit();
 	return 0;
