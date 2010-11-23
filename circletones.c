86d260f18b8719996a800bf95eb4a4e0c3d61343
Christopher Vollick <psycotica0@gmail.com>
2010-11-22 22:40:32 -0500

    Multiple Circles Colliding and Stuff.
    Sweet!
    The groundwork is all here.
    Now I just need to make it so that a new circle is spawned under the mouse click.
    
    And, you know, sounds...

--- circletones.c
+++ circletones.c
@@ -6,35 +6,143 @@
 #include "SDL/SDL_gfxPrimitives.h"
 #include "SDL/SDL_framerate.h"
 
-void drawFrame(SDL_Surface* screen, int radius) {
-	static const int x = 320;
-	static const int y = 240;
-	static const Uint32 borderColour = 0x000000FF;
-	static const Uint32 fillColour = 0xAA00AAFF;
-	static const Uint32 backgroundColour = 0xAA44AAFF;
+typedef struct Circle {
+	int x;
+	int y;
+	int radius;
+	int borderWidth;
+	int rate;
+	Uint32 borderColour;
+	Uint32 fillColour;
+} Circle;
 
-	/* For no reason the filledRectangle is called box */
-	boxColor(screen, 0, 0, 640, 480, backgroundColour);
+typedef struct CircleNode {
+	Circle* circle;
+	struct CircleNode* next;
+} CircleNode;
 
-	filledCircleColor(screen, x, y, radius + 4, borderColour);
-	filledCircleColor(screen, x, y, radius, fillColour);
+typedef struct CircleList {
+	int size;
+	struct CircleNode* head;
+} CircleList;
+
+CircleNode* newCircle(CircleList* list, int x, int y) {
+	CircleNode* temp;
+
+	temp = malloc(sizeof(CircleNode));
+	if (temp == NULL)
+		return NULL;
+
+	temp->circle = malloc(sizeof(Circle));
+	if (temp->circle == NULL) {
+		free(temp);
+		return NULL;
+	}
+
+	temp->circle->x = x;
+	temp->circle->y = y;
+
+	temp->circle->rate = 1;
+	temp->circle->radius = 1;
+	temp->circle->borderWidth = 2;
+	temp->circle->borderColour = 0x000000FF;
+	temp->circle->fillColour = 0xAA00AAFF;
+
+	temp->next = list->head;
+	list->head = temp;
+
+	list->size++;
+
+	return temp;
 }
 
+CircleList* initializeCircleList() {
+	CircleList* temp;
+
+	temp = malloc(sizeof(CircleList));
+	temp->head = NULL;
+	temp->size = 0;
+
+	return temp;
+}
+
+/* This function returns the absolute distance between the centres of circle1 and circle2 */
+int calculateAbsDistance(Circle* circle1, Circle* circle2) {
+	int x = circle1->x - circle2->x;
+	int y = circle1->y - circle2->y;
+	return (int)sqrt(x*x + y*y);
+}
+
+void doCollisionNode(CircleNode* head) {
+	CircleNode* current;
+	for (current = head->next; current != NULL; current=current->next) {
+		if (calculateAbsDistance(head->circle, current->circle) <= (head->circle->radius + current->circle->radius)) {
+			/* They're hitting */
+			if (head->circle->rate >= 0) {
+				/* If it's expanding, make it shrink */
+				head->circle->rate *= -1;
+			}
+
+			if (current->circle->rate >= 0) {
+				/* Make it shrink too */
+				current->circle->rate *= -1;
+			}
+		}
+	}
+}
+
+void doRebound(Circle* current) {
+	if (current->radius <= 1 && current->rate <= 0) {
+		current->rate *= -1;
+	}
+}
+
+void drawCircle(SDL_Surface* screen, Circle* circle) {
+	/* Border */
+	if (circle->borderWidth > 0)
+		filledCircleColor(screen, circle->x, circle->y, circle->radius + circle->borderWidth, circle->borderColour);
+	/* Fill */
+	filledCircleColor(screen, circle->x, circle->y, circle->radius, circle->fillColour);
+}
+
+void updateCircle(Circle* circle) {
+	circle->radius += circle->rate;
+}
+
+void drawFrame(SDL_Surface* screen, CircleList* list) {
+	CircleNode* current;
+	Uint32 backgroundColour = 0xAA44AAFF;
+	/* For no reason the filledRectangle is called box */
+	/* Anyway, clear the screen */
+	boxColor(screen, 0, 0, 640, 480, backgroundColour);
+	for (current = list->head; current != NULL; current = current->next) {
+		drawCircle(screen, current->circle);
+		doRebound(current->circle);
+		doCollisionNode(current);
+		updateCircle(current->circle);
+	}
+}
 #define FPS 60
 
 int main (int argc, char** argv) {
 	SDL_Surface *screen;
 	FPSmanager fpsm;
+	CircleList* list;
 	long i;
 
 	SDL_Init(SDL_INIT_VIDEO);
+	list = initializeCircleList();
 
 	screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE | SDL_DOUBLEBUF);
 	SDL_initFramerate(&fpsm);
 	SDL_setFramerate(&fpsm, FPS);
 
-	for(i=0; i <= FPS * 4; i++) {
-		drawFrame(screen, i);
+	newCircle(list, 14, 32);
+	newCircle(list, 340, 98);
+	newCircle(list, 160, 400);
+
+	for(i=0; i <= FPS * 30; i++) {
+		drawFrame(screen, list);
 		SDL_Flip(screen);
 		SDL_framerateDelay(&fpsm);
 	}
